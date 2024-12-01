0dc4cffd2c6607c6440ec80e8e4104631bf50508
HemanthKumarPutta <phk99913@gmail.com>
2024-12-01 20:12:01 +0530

    init commit

--- src/main/java/com/example/lowleveldesign_patterns/factory/refactoring_guru/Button/Button.java
+++
@@ -1,6 +0,0 @@
-package com.example.lowleveldesign_patterns.factory.refactoring_guru.Button;
-
-public interface Button {
-    public void OnClick();
-    public void render();
-}
--- src/main/java/com/example/lowleveldesign_patterns/factory/refactoring_guru/Dialog/ButtonDialog.java
+++
@@ -1,7 +0,0 @@
-package com.example.lowleveldesign_patterns.factory.refactoring_guru.Dialog;
-
-import com.example.lowleveldesign_patterns.factory.refactoring_guru.Button.Button;
-
-public abstract class ButtonDialog {
-    public abstract Button createButton();
-}
